08bee05aa3a924437e28214b73d56b6f507a57c1
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-20 14:47:38 +0300

    add className to Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return (<nav className={d.nav}>
-            <div className={`${d.item} ${d.active}`}><NavLink to={"profile"}>Profile</NavLink></div>
-            <div className={`${d.item} ${d.active}`}><NavLink to={"dialogs"}>Messages</NavLink></div>
-            <div className={`${d.item} ${d.active}`}><NavLink to={"news"}>News</NavLink></div>
-            <div className={`${d.item} ${d.active}`}><NavLink to={"music"}>Music</NavLink></div>
-            <div className={`${d.item} ${d.active}`}><NavLink to={"settings"}>Settings</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"profile"} className={navData=>navData.isActive ? d.active: d.item}>Profile</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"dialogs"} className={navData=>navData.isActive ? d.active: d.item}>Messages</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"news"} className={navData=>navData.isActive ? d.active: d.item}>News</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"music"} className={navData=>navData.isActive ? d.active: d.item}>Music</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"settings"} className={navData=>navData.isActive ? d.active: d.item}>Settings</NavLink></div>
 
         </nav>
 
