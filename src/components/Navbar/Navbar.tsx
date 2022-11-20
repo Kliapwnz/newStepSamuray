9d5a3d1293feec62babe6ae63bde01fb8bed69ca
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-20 14:37:07 +0300

    change <a> to <NavLink>

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import d from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return (<nav className={d.nav}>
-            <div className={`${d.item} ${d.active}`}><a href={"profile"}>Profile</a></div>
-            <div className={`${d.item} ${d.active}`}><a href={"dialogs"}>Messages</a></div>
-            <div className={`${d.item} ${d.active}`}><a href={"news"}>News</a></div>
-            <div className={`${d.item} ${d.active}`}><a href={"music"}>Music</a></div>
-            <div className={`${d.item} ${d.active}`}><a href={"settings"}>Settings</a></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"profile"}>Profile</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"dialogs"}>Messages</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"news"}>News</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"music"}>Music</NavLink></div>
+            <div className={`${d.item} ${d.active}`}><NavLink to={"settings"}>Settings</NavLink></div>
 
         </nav>
 
