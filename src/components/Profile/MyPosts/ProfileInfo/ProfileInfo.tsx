01921ab04e5509f3c68f1650f2f30becb8ae673e
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-25 12:50:04 +0300

    add ProfileInfo.tsx

---
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"/>
+            </div>
+            </div>
+    );
+};
+
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import {MyPosts} from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./MyPosts/ProfileInfo/ProfileInfo";
 
 export const Profile = () => {
     return (
         <div>
-            <div>
-                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"/>
-            </div>
+            <ProfileInfo/>
             <div>
                 ava + description
             </div>
