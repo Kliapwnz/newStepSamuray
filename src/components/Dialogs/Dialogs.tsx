e44d5b8a8f5a073d3c1322e03b98e5ba7957dc21
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-25 15:12:01 +0300

    add dialogsData[]

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -9,7 +9,7 @@ type dialogItemPropsType = {
     name: string
     id: number
 }
-const DialogItem = (props:dialogItemPropsType) => {
+const DialogItem = (props: dialogItemPropsType) => {
     let path = "/dialogs/" + props.id
 
     return (<div className={s.dialog}>
@@ -19,28 +19,38 @@ const DialogItem = (props:dialogItemPropsType) => {
     )
 }
 
-const Message = (props:messagePropsType) => {
-    return(
+const Message = (props: messagePropsType) => {
+    return (
         <div className={s.message}>{props.message}</div>
     )
 }
+
+let dialogsData = [
+    {id: 1, name: "Anastasiya"},
+    {id: 2, name: "Natasha"},
+    {id: 3, name: "Artem"},
+    {id: 4, name: "Sasha"},
+    {id: 5, name: "Evgeniy"},
+    {id: 6, name: "Yuri"}
+]
+
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <DialogItem id={1} name="Anastasiya" />
-                <DialogItem id={2} name="Natasha" />
-                <DialogItem id={3} name="Artem" />
-                <DialogItem id={4} name="Sasha" />
-                <DialogItem id={5} name="Evgeniy" />
-                <DialogItem id={6} name="Yuri" />
+                <DialogItem id={1} name="Anastasiya"/>
+                <DialogItem id={2} name="Natasha"/>
+                <DialogItem id={3} name="Artem"/>
+                <DialogItem id={4} name="Sasha"/>
+                <DialogItem id={5} name="Evgeniy"/>
+                <DialogItem id={6} name="Yuri"/>
 
             </div>
             <div className={s.messages}>
-               <Message message="Hi"/>
-               <Message message="How are you?"/>
-               <Message message="Have a great day!"/>
+                <Message message="Hi"/>
+                <Message message="How are you?"/>
+                <Message message="Have a great day!"/>
             </div>
         </div>
     )
