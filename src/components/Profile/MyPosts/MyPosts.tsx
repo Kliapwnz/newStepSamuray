956fbef6432b1da6d5af1c89be896d616552a19a
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-25 13:00:23 +0300

    Myposts padding

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,7 +4,7 @@ import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
     return (
-        <div>
+        <div className={w.postsStyle}>
             <div>
                 my posts
                 <div>
