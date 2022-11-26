a9bd5701b94a4ebf8d8e9026ff41a5c3e2d16465
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-26 13:35:19 +0300

    nice looks

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -8,7 +8,6 @@ type dialogItemPropsType = {
 }
 
 
-
 export const DialogItem = (props: dialogItemPropsType) => {
     let path = "/dialogs/" + props.id
 
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,28 +3,25 @@ import {Message} from "./Message/Message";
 import {DialogItem} from "./DialogItem/DialogItem";
 
 
-
-
-
 let
- dialogsData = [
-    {id: 1, name: "Anastasiya"},
-    {id: 2, name: "Natasha"},
-    {id: 3, name: "Artem"},
-    {id: 4, name: "Sasha"},
-    {id: 5, name: "Evgeniy"},
-    {id: 6, name: "Yuri"}
-]
+    dialogsData = [
+        {id: 1, name: "Anastasiya"},
+        {id: 2, name: "Natasha"},
+        {id: 3, name: "Artem"},
+        {id: 4, name: "Sasha"},
+        {id: 5, name: "Evgeniy"},
+        {id: 6, name: "Yuri"}
+    ]
 
 let messagesData = [
-    {id:1, message: "Hi"},
-    {id:2, message: "How are you?"},
-    {id:3, message: "Manchester city"}
+    {id: 1, message: "Hi"},
+    {id: 2, message: "How are you?"},
+    {id: 3, message: "Manchester city"}
 ]
 
-let dialogMap = dialogsData.map(d=><DialogItem id={d.id} name={d.name}/>)
+let dialogMap = dialogsData.map(d => <DialogItem id={d.id} name={d.name}/>)
 
-let messageMap = messagesData.map(m=><Message message={m.message}/>)
+let messageMap = messagesData.map(m => <Message message={m.message}/>)
 
 export const Dialogs = () => {
     return (
